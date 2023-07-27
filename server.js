cb2bbb155633c70514db779e1206bcb4ef9496d7
mrkteq <markanthonytucker@gmail.com>
2023-07-27 20:56:53 +0200

    changed to ext url

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image  = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgres://smart_brain_xy64_user:quC1BbE7iNfwpEVKEuZsbWBr7VOfuEOE@dpg-cj0g8gs07spl5op4us50-a/smart_brain_xy64',
+      host : 'postgres://smart_brain_xy64_user:quC1BbE7iNfwpEVKEuZsbWBr7VOfuEOE@dpg-cj0g8gs07spl5op4us50-a.frankfurt-postgres.render.com/smart_brain_xy64',
       port : 5432,
       user : 'smart_brain_xy64_user',
       password : 'quC1BbE7iNfwpEVKEuZsbWBr7VOfuEOE',
