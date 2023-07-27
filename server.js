2650491a53a920d18409bee9019abfbb38297509
mrkteq <markanthonytucker@gmail.com>
2023-07-27 20:06:31 +0200

    changes the response again

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(cors());
 //     res.send(db.users);
 // });
 
-app.post('/', (req, res) => { res.send('this is working') });
+app.get('/', (req, res) => { res.send('this is working') });
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt) });
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt) });
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db) });
