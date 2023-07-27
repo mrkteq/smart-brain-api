52929737f48f5d6e61c80a038ebd629c5898aeb0
mrkteq <markanthonytucker@gmail.com>
2023-07-27 19:20:12 +0200

    added the port var

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcryp
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db) });
 app.put('/image', (req, res) => {image.handleImage(req, res, db) });
 
-app.listen(3000, () => {
-    console.log('smart brain app listening on port 3000!');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('smart brain app listening on port ${process.env.PORT}!');
 });
 
 /*
